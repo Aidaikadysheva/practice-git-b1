baf5dc49a78abc4566052e4ec0d4b60b4ab195a4
AILA <aidaikadysheva@gmail.com>
2023-05-11 14:13:04 -0500

    my code

---
+++ src/main/java/Apple.java
@@ -0,0 +1,13 @@
+public class Apple {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("HELLOOOO");
+        System.out.println("hello code");
+    }
+
+    public static void welcome(){
+        System.out.println("Welcome to Dubai");
+    }
+}
